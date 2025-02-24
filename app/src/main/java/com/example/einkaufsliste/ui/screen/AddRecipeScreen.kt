7a124cc18a36ec68d2a499c2b3b1f189df2c1e5b
jeffreyk <j.kahlert@ostfalia.de>
2025-02-24 19:10:03 +0100

    Add: Button to AddRecipeScreen

--- app/src/main/java/com/example/einkaufsliste/ui/screen/AddRecipeScreen.kt
+++ app/src/main/java/com/example/einkaufsliste/ui/screen/AddRecipeScreen.kt
@@ -1,38 +1,40 @@
 package com.example.einkaufsliste.ui.screen
 
+import android.widget.Space
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.Divider
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.einkaufsliste.R
 import com.example.einkaufsliste.ui.theme.EinkaufslisteTheme
+import com.example.einkaufsliste.ui.theme.Shapes
 import com.example.einkaufsliste.ui.viewmodel.AddRecipeViewModel
 import kotlinx.coroutines.flow.map
 
@@ -47,12 +49,20 @@ fun AddRecipeScreen(
     ) {
         AddRecipeTopBar()
         HorizontalDivider(thickness = 2.dp)
-        EnterTextComposable(viewModel,"Name")
-        EnterTextComposable(viewModel,"Zutaten")
-        EnterTextComposable(viewModel, "Anweisung")
-    }
-}
+        Column(
+            modifier = Modifier
+                .verticalScroll(rememberScrollState())
+        ) {
+            EnterTextComposable(viewModel, "Name")
+            EnterTextComposable(viewModel, "Zutaten")
+            EnterTextComposable(viewModel, "Anweisung")
 
+        }
+        Spacer(Modifier.weight(1f))
+        SaveAndCancelButtons()
+    }
+
+}
 
 @Composable
 fun EnterTextComposable(
@@ -88,7 +98,9 @@ fun EnterTextComposable(
                 checkInput(textFieldName, it, viewModel)
             },
             label = { Text("")},
-            modifier = Modifier.fillMaxWidth().padding(top = 4.dp)
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 4.dp)
         )
     }
 }
@@ -101,29 +113,15 @@ fun checkInput(field: String, value: String, viewModel: AddRecipeViewModel) {
     }
 }
 
-@Preview
-@Composable
-fun AddRecipeScreenLightTheme() {
-    EinkaufslisteTheme(darkTheme = false) {
-        Scaffold(
-            modifier = Modifier.fillMaxSize()
-        ) { innerPadding ->
-            AddRecipeScreen(Modifier.padding(innerPadding))
-        }
-    }
-}
-
 @Composable
 fun AddRecipeTopBar(
     modifier: Modifier = Modifier
 ) {
     Row(
+        modifier = Modifier.fillMaxWidth(),
         verticalAlignment = Alignment.CenterVertically
     ) {
-        Button(
-            onClick = {},
-            colors = ButtonDefaults.buttonColors(Color.Transparent)
-        ) {
+        IconButton(onClick = {}) {
             Icon(
                 imageVector = Icons.Filled.ArrowBack,
                 contentDescription = "",
@@ -132,10 +130,91 @@ fun AddRecipeTopBar(
         }
         Spacer(Modifier.weight(1f))
         Text(
-            text= "Rezept",
+            text = "Rezept",
             style = MaterialTheme.typography.displayLarge,
-            modifier = Modifier.padding(16.dp)
+            modifier = Modifier
+                .padding(16.dp)
         )
+
+    }
+}
+
+/*@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AddRecipeTopBar(
+    modifier: Modifier = Modifier
+) {
+    TopAppBar(
+        title = {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Spacer(Modifier.weight(1f))
+                Text(
+                    text= "Rezept",
+                    style = MaterialTheme.typography.displayLarge,
+                    modifier = Modifier
+                        .padding(16.dp)
+                )
+            }
+        },
+        navigationIcon = {
+            IconButton(onClick = {}) {
+                Icon(
+                    imageVector = Icons.Filled.ArrowBack,
+                    contentDescription = "",
+                    tint = MaterialTheme.colorScheme.onPrimaryContainer
+                )
+            }
+        }
+    )
+}*/
+
+@Composable
+fun SaveAndCancelButtons(
+    modifier: Modifier = Modifier
+) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(dimensionResource(R.dimen.padding_medium)),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+
+        Box(
+            modifier = Modifier.weight(0.5f)
+        ) {
+            Button(
+                onClick = {},
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(dimensionResource(R.dimen.padding_small)),
+                shape = Shapes.medium
+            ) {
+                Text(
+                    text = "Abbrechen",
+                    style = MaterialTheme.typography.displayMedium
+                )
+            }
+        }
+
+        Box(
+            modifier = Modifier.weight(0.5f),
+        ) {
+            Button(
+                onClick = {},
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(dimensionResource(R.dimen.padding_small)),
+                shape = Shapes.medium
+            ) {
+                Text(
+                    text = "Speichern",
+                    style = MaterialTheme.typography.displayMedium
+                )
+            }
+        }
     }
 }
 
@@ -143,6 +222,18 @@ fun AddRecipeTopBar(
 @Composable
 fun AddRecipeScreenDarkTheme() {
     EinkaufslisteTheme(darkTheme = true) {
+        Scaffold(
+            modifier = Modifier.fillMaxSize()
+        ) { innerPadding ->
+            AddRecipeScreen(Modifier.padding(innerPadding))
+        }
+    }
+}
+
+@Preview
+@Composable
+fun AddRecipeScreenLightTheme() {
+    EinkaufslisteTheme(darkTheme = false) {
         Scaffold(
             modifier = Modifier.fillMaxSize()
         ) { innerPadding ->
